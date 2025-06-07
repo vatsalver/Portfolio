395eefb98344c58fb521b3c61f441d513dd47f7f
deathrack <127497796+vatsalver@users.noreply.github.com>
2025-06-07 17:33:31 +0530

    remove cn

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
-import { cn } from "@/lib/utils";
+//import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -31,10 +31,9 @@ export const ThemeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
-      )}
+      className={
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 focus:outlin-hidden"
+      }
     >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
